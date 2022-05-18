2ef3653cfd49ed2e8423bfeb2fe2682597b4761c
spy86 <contact@mmichalski.com>
2022-05-18 17:00:04 +0200

    update

--- servicebus_queue_standard_output.tf
+++ servicebus_queue_standard_output.tf
@@ -40,37 +40,37 @@ output "secondary_listen_connection_string" {
   sensitive = false
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.standard_listen_auth_rule" ]
 }
-output "primary_listen_shared_access_key" {
+output "standard_primary_listen_shared_access_key" {
   description = "The Primary Key for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.standard_listen_auth_rule.primary_key}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.standard_listen_auth_rule" ]
 }
-output "secondary_listen_shared_access_key" {
+output "standard_secondary_listen_shared_access_key" {
   description = "The Secondary Key for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.standard_listen_auth_rule.secondary_key}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.standard_listen_auth_rule" ]
 }
-output "primary_manage_connection_string" {
+output "standard_primary_manage_connection_string" {
   description = "The Primary Connection String for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule.primary_connection_string}"
   sensitive = false
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule" ]
 }
-output "secondary_manage_connection_string" {
+output "standard_secondary_manage_connection_string" {
   description = "The Secondary Connection String for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule.secondary_connection_string}"
   sensitive = false
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule" ]
 }
-output "primary_manage_shared_access_key" {
+output "standard_primary_manage_shared_access_key" {
   description = "The Primary Key for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule.primary_key}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule" ]
 }
-output "secondary_manage_shared_access_key" {
+output "standard_secondary_manage_shared_access_key" {
   description = "The Secondary Key for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.standard_manage_auth_rule.secondary_key}"
   sensitive = true

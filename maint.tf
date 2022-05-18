da4f58fb5879f207b4f1c4aac675d937d5e649dc
spy86 <contact@mmichalski.com>
2022-05-18 17:11:35 +0200

    missing data

--- maint.tf
+++ maint.tf
@@ -3,3 +3,8 @@ data "azurerm_client_config" "current" {}
 data "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}"
 }
+
+data "azurerm_servicebus_namespace" "main" {
+    name                = "${var.servicebus_namespace_name}"
+    resource_group_name = "${data.azurerm_resource_group.rg.name}"
+  }

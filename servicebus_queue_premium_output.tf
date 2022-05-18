d965ad88ee740e3dcae35630da251b06705a0f13
spy86 <contact@mmichalski.com>
2022-05-18 17:06:46 +0200

    update

--- servicebus_queue_premium_output.tf
+++ servicebus_queue_premium_output.tf
@@ -1,40 +1,40 @@
-output "name" {
+output "premium_name" {
   description = "Specifies the name of the ServiceBus Queue resource"
   value = "${azurerm_servicebus_queue.main.name}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule" ]
 }
-output "primary_send_connection_string" {
+output "premium_primary_send_connection_string" {
   description = "The Primary Connection String for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule.primary_connection_string}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule" ]
 }
-output "secondary_send_connection_string" {
+output "premium_secondary_send_connection_string" {
   description = "The Secondary Connection String for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule.secondary_connection_string}"
   sensitive = false
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule" ]
 }
-output "primary_send_shared_access_key" {
+output "premium_primary_send_shared_access_key" {
   description = "The Primary Key for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule.primary_key}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule" ]
 }
-output "secondary_send_shared_access_key" {
+output "premium_secondary_send_shared_access_key" {
   description = "The Secondary Key for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule.secondary_key}"
   sensitive = true
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.premium_send_auth_rule" ]
 }
-output "primary_listen_connection_string" {
+output "premium_primary_listen_connection_string" {
   description = "The Primary Connection String for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.premium_listen_auth_rule.primary_connection_string}"
   sensitive = false
   depends_on = [ "azurerm_servicebus_queue_authorization_rule.premium_listen_auth_rule" ]
 }
-output "secondary_listen_connection_string" {
+output "premium_secondary_listen_connection_string" {
   description = "The Secondary Connection String for the Authorization Rule."
   value = "${azurerm_servicebus_queue_authorization_rule.premium_listen_auth_rule.secondary_connection_string}"
   sensitive = false
